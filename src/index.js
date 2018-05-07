57c320e949739d3be2d70a36131334e10302a3d1
eldimious <botsaris.d@gmail.com>
2018-05-07 20:34:26 +0300

    export http errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-const test = require('./httpErrors');
-console.log('1111', test)
-console.log('test', new test.NotFoundError(`FB Page with id: not found`, 'PAGE_NOT_FOUND'))
+module.exports = require('./httpErrors');
