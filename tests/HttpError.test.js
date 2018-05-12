9b89b32393018e58e65b591993b0485c6879e014
eldimious <botsaris.d@gmail.com>
2018-05-12 16:03:11 +0300

    improve lib files styling

--- tests/HttpError.test.js
+++ tests/HttpError.test.js
@@ -1,6 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const errors = require('../src');
+const errors = require('../lib');
 
 const getDataFromDB = function getDataFromDB() {
   throw new errors.NotFound();
